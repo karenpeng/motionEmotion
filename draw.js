8feb3125e412e8a4af4121a38a1e896a4e318102
Karen <karenpenglabs@gmail.com>
2014-09-06 18:50:48 -0400

    make shapes last for a while

--- draw.js
+++ draw.js
@@ -1,14 +1,49 @@
 (function (exports) {
 
+  function MyTriangle(a, b, c) {
+    this.a = a;
+    this.b = b;
+    this.c = c;
+    this.hue = Math.floor(Math.random() * 360);
+    this.lightness = Math.floor(Math.random() * 50 + 50);
+    this.alpha = 0.9;
+    this.lightUp = false;
+  }
+  MyTriangle.prototype.update = function () {
+    this.alpha -= 0.002;
+    return this.alpha > 0;
+  };
+
+  MyTriangle.prototype.render = function (ctx) {
+    var saturation = '100%';
+    if (!this.lightUp) {
+      ctx.fillStyle = "hsla(" + this.hue.toString() + "," + saturation + "," +
+        this.lightness.toString() +
+        "%" + "," + this.alpha + ")";
+    } else {
+      ctx.fillStyle = "hsla(" + this.hue.toString() + "," + saturation + "," +
+        this.lightness.toString() +
+        "%" + ")";
+    }
+    ctx.strokeStyle = ctx.fillStyle;
+    ctx.beginPath();
+    ctx.moveTo(this.a[0], this.a[1]);
+    ctx.lineTo(this.b[0], this.b[1]);
+    ctx.lineTo(this.c[0], this.c[1]);
+    ctx.closePath();
+    ctx.stroke();
+    ctx.fill();
+  };
+
   function DrawPoints(can) {
     this.can = can;
     this.ctx = this.can.getContext('2d');
     this.width = this.can.width;
     this.height = this.can.height;
-    this.triangles;
+    this.myTriangles = [];
   }
 
-  DrawPoints.prototype.getPoints = function (points, width, height) {
+  DrawPoints.prototype.updatePoints = function (points, width, height) {
     this.widthRatio = this.width / width;
     this.heightRatio = this.height / height;
     this.vertices = [];
@@ -17,12 +52,19 @@
     }
   };
 
-  DrawPoints.prototype.draw = function () {
-    // this.points.forEach(function(p){
-    // 	p.render(this.ctx, 2);
-    // });
+  DrawPoints.prototype.makeTriangle = function () {
     this.triangles = Delaunay.triangulate(this.vertices);
+    this.myTriangles = [];
+    for (var i = this.triangles.length; i >= 3; i -= 3) {
+      this.myTriangles.push(new MyTriangle(
+        this.vertices[this.triangles[i - 1]],
+        this.vertices[this.triangles[i - 2]],
+        this.vertices[this.triangles[i - 3]]
+      ));
+    }
+  };
 
+  DrawPoints.prototype.draw = function () {
     this.ctx.save();
     this.ctx.translate(this.width, 0);
     this.ctx.scale(-1, 1);
@@ -31,6 +73,14 @@
     }
     this.ctx.restore();
 
+    if (this.myTriangles.length > 0) {
+      for (var l = 0; l < this.myTriangles.length; l++) {
+        if (this.myTriangles[l].update()) {
+          this.myTriangles[l].render(this.ctx);
+        }
+      }
+    }
+    /*
     for (var l = this.triangles.length; l >= 3; l -= 3) {
       var hue = Math.floor(Math.random() * 360);
       var saturation = '100%';
@@ -56,6 +106,7 @@
       //this.ctx.stroke();
       this.ctx.fill();
     }
+    */
   }
 
   // function Point(x, y) {
--- main.js
+++ main.js
@@ -7,23 +7,61 @@ var drawPoints = new DrawPoints(myCanvas);
 
 var data = new Data();
 
+var timer = 0;
+var startCounting = false;
+var maxThreshold = 18;
+
 function setup() {
   detectPoints.init();
   //cornerDetect.init();
+  setTimeout(function () {
+    start = true;
+  }, 500);
 }
 
 function update() {
+
   detectPoints.draw();
-  drawPoints.getPoints(detectPoints.points, detectPoints.width, detectPoints.height);
+
+  if (detectPoints.points.length < maxThreshold) {
+    if (detectPoints.points.length < 3) {
+      //console.log("no movement :(");
+    } else {
+      drawPoints.updatePoints(detectPoints.points, detectPoints.width,
+        detectPoints.height);
+      drawPoints.makeTriangle();
+    }
+  }
+
+  /*
+  if (timer === 0) {
+    if (detectPoints.points.length > 0) {
+      drawPoints.updatePoints(detectPoints.points, detectPoints.width,
+        detectPoints.height);
+      drawPoints.makeTriangle();
+      startCounting = true;
+    }
+  }
+  //start = false;
+  if (startCounting) {
+    timer++;
+  }
+  if (timer > 250) {
+    startCounting = false;
+    timer = 0;
+  }
+*/
   drawPoints.draw();
+  /*
   data.getPoints(drawPoints.vertices);
   //console.log(drawPoints.vertices.length)
   data.getAvg();
   data.getTotalDist();
   //console.log(data.points.length, data.avgX, data.avgY)
-  // console.log(data.totalDist)
+  //console.log(data.totalDist)
 
   //cornerDetect.tick();
+  */
 }
 
 function loop(callback) {
