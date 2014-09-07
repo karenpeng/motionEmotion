ab152739173e88c7e5c7efb73be63c3104a17b6c
Jason Sigal <jason.sigal@gmail.com>
2014-09-07 04:09:08 -0400

    adding emo.js to track emotions from facial expressions

---
+++ emo.js
@@ -0,0 +1,36 @@
+// thank you Audun Mathias Øygard for clmtrackr!
+// http://auduno.github.io/clmtrackr/examples/clm_emotiondetection.html
+
+var ctrack = new clm.tracker({useWebGL : true});
+
+ctrack.init(pModel);
+
+var vid = document.getElementsByTagName('video')[0];
+ctrack.start(vid);
+
+var ec = new emotionClassifier();
+ec.init(emotionModel);
+var emotionData = ec.getBlank();
+
+var currentEmo;
+var prevEmo;
+
+// called by animation loop
+function emoLoop() {
+  var cp = ctrack.getCurrentParameters();
+  var er = ec.meanPredict(cp);
+  var maxEmo = null;
+  var maxVal = 0.5;
+  for (var i in er) {
+    if (er[i].value > maxVal) {
+      maxEmo = er[i].emotion;
+      maxVal = er[i].value;
+    }
+  }
+  if (maxEmo !== prevEmo) {
+    setScale(maxEmo);
+    currentEmo = maxEmo;
+    prevEmo = maxEmo;
+    console.log(currentEmo + maxVal);
+  }
+}
